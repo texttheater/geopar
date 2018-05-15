772c5dd8049a0f1481d4399fee6fdc2128dcc868
Kilian Evang <noreply@texttheater.net>
2018-05-15 21:19:13 +0200

    Converted check script to unittest

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Made available under the GPLv2 license.
 The data included is also GPLv2 and was obtained from
 http://www.cs.utexas.edu/users/ml/nldata/geoquery.html. The split into training
 and testing portions is the same as in Tom Kwiatkowski’s UBL system.
+
+How to Run the Test Suite
+-------------------------
+
+    python3 -m unittest
---
+++ test_lexicon_coverage.py
@@ -0,0 +1,48 @@
+"""This program checks the coverage of GeoPar's lexicon.
+
+For each NLU-MR pair in the training data, for each lexical term in the MR, it
+checks whether there is a word or multiword (up to length 2) in the NLU that in
+the lexicon is associated with that lexical term. Prints a warning if not.
+"""
+
+
+import data
+import lexicon
+import terms
+import itertools
+import unittest
+import util
+
+
+class LexiconCoverageTestCase(unittest.TestCase):
+
+    def test(self):
+        """Tests the coverage of GeoPar's lexicon.
+
+        For each MLU-NR pair in the training data, for each lexical term in the
+        MR, it checks whether there is a word or multiword (up to length 3) in
+        the NLU that in the lexicon is associated with that lexical term.
+        """
+        missing_terms = []
+        for words, reader, mr in data.geo880_train():
+            printed = False
+            self.assertEqual(mr.functor_name, 'answer')
+            self.assertEqual(len(mr.args), 2)
+            self.assertIsInstance(mr.args[0], terms. Variable)
+            for lexterm in mr.args[1].lexterms():
+                word_found = False
+                unigrams = util.ngrams(1, words)
+                bigrams = util.ngrams(2, words)
+                trigrams = util.ngrams(3, words)
+                for word in itertools.chain(unigrams, bigrams, trigrams):
+                    for term in lexicon.meanings(word):
+                        if term.equivalent(lexterm):
+                            word_found = True
+                            break
+                if not word_found:
+                    if not printed:
+                        print(str(words))
+                        printed = True
+                    print('WARNING: no word found that means ' + lexterm.to_string(reader))
+                    missing_terms.append(lexterm)
+        self.assertEqual(missing_terms, [])
