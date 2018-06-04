69e7a5beeff0d222ad0af29957d930a671196cc9
Kilian Evang <noreply@texttheater.net>
2018-06-04 21:49:29 +0200

    Items can now generate their successors

--- parseitems.py
+++ parseitems.py
@@ -1,6 +1,12 @@
+import lexicon
 import lstack
 import parsestacks
 import terms
+
+
+MAX_TOKEN_LENGTH = 3
+MAX_SECSTACK_DEPTH = 1
+MAX_ARGS = 3
 
 
 def initial(words):
@@ -22,13 +28,11 @@ class ParseItem:
         self.finished = finished
 
     def skip(self):
-        assert not self.finished
         stack = self.stack
         queue = self.queue.pop()
         return ParseItem(stack, queue, False)
 
     def shift(self, n, term):
-        assert not self.finished
         se = parsestacks.StackElement(term)
         stack = self.stack.push(se)
         queue = self.queue
@@ -37,14 +41,12 @@ class ParseItem:
         return ParseItem(stack, queue, False)
 
     def coref(self, i, j, k, l):
-        assert not self.finished
         old = self.stack[1].arg(i, j)
         new = self.stack[0].arg(k, l)
         stack = lstack.stack(se.replace(old, new) for se in self.stack)
         return ParseItem(stack, self.queue, False)
 
     def drop(self, i, j):
-        assert not self.finished
         stack = self.stack
         droppee = stack.head.term
         stack = stack.pop()
@@ -55,7 +57,6 @@ class ParseItem:
         return ParseItem(stack, self.queue, False)
 
     def lift(self, i, j):
-        assert not self.finished
         stack = self.stack
         se_old = stack.head
         stack = stack.pop()
@@ -74,3 +75,90 @@ class ParseItem:
     def idle(self):
         assert self.finished
         return self
+
+    def successors(self):
+        """Returns all possible successors.
+
+        More exactly, returns a dictionary whose keys are the possible actions
+        on this item and the values are the resulting successor items.
+
+        Subject to hard-coded maximum arguments for shift, coref, drop, lift
+        actions.
+        """
+        return dict(self._successors())
+
+    def _successors(self):
+        # skip
+        if not self.queue.is_empty():
+            action = ('skip',)
+            succ = self.skip()
+            yield action, succ
+        # shift
+        for token_length in range(1, MAX_TOKEN_LENGTH):
+            try:
+                token = tuple(self.queue[i] for i in range(token_length))
+            except IndexError: # queue too short
+                break
+            for meaning in lexicon.meanings(token):
+                action = ('shift', token_length, meaning.to_string())
+                succ = self.shift(token_length, meaning)
+                yield action, succ
+        # coref
+        for i in range(MAX_SECSTACK_DEPTH + 1):
+            for j in range(1, MAX_ARGS + 1):
+                for k in range(MAX_SECSTACK_DEPTH + 1):
+                    for l in range(1, MAX_ARGS + 1):
+                        action = ('coref', i, j, k, l)
+                        try:
+                            succ = self.coref(i, j, k, l)
+                        except IndexError:
+                            continue
+                        yield action, succ
+        # drop
+        for i in range(MAX_SECSTACK_DEPTH + 1):
+            for j in range(1, MAX_ARGS + 1):
+                action = ('drop', i, j)
+                try:
+                    succ = self.drop(i, j)
+                except IndexError:
+                    continue
+                yield action, succ
+        # lift
+        for i in range(MAX_SECSTACK_DEPTH + 1):
+            for j in range(1, MAX_ARGS + 1):
+                action = ('lift', i, j)
+                try:
+                    succ = self.lift(i, j)
+                except IndexError:
+                    continue
+                yield action, succ
+        # finish
+        if not self.finished and len(self.stack) == 1 and self.queue.is_empty():
+            action = ('finish',)
+            succ = self.finish()
+            yield action, succ
+        # idle
+        if self.finished:
+            action = ('idle',)
+            succ = self.idle()
+            yield action, succ
+
+    def successor(self, action, *args):
+        if action == 'skip':
+            return self.skip(*args)
+        elif action == 'shift':
+            length, meaning = args
+            meaning = terms.from_string(meaning)
+            return self.shift(length, meaning)
+        elif action == 'coref':
+            return self.coref(*args)
+        elif action == 'drop':
+            return self.drop(*args)
+        elif action == 'lift':
+            return self.lift(*args)
+        elif action == 'finish':
+            return self.finish(*args)
+        elif action == 'idle':
+            return self.idle(*args)
+        else:
+            raise ValueError('invalid action: ' + action)
--- test_parseitems.py
+++ test_parseitems.py
@@ -54,3 +54,42 @@ class ItemsTestCase(unittest.TestCase):
         self.assertTrue(item.finished)
         self.assertTrue(item.stack.head.term.equivalent(terms.from_string(
             'answer(C, (capital(S, C), largest(P, (state(S), population(S, P)))))')))
+
+    def test_successors(self):
+        """Tests the successor(s) methods.
+
+        Same as above, but this time we generate all items using the successors
+        method and make sure that it generates all the correction actions/items.
+        """
+        item = parseitems.initial(('what', 'is', 'the', 'capital', 'of', 'the', 'state', 'with', 'the', 'largest', 'population'))
+        actions = [
+                ('skip',),
+                ('skip',),
+                ('skip',),
+                ('shift', 1, 'capital(A,B)'),
+                ('coref', 0, 1, 0, 2),
+                ('drop', 0, 2),
+                ('skip',),
+                ('skip',),
+                ('shift', 1, 'state(A)'),
+                ('coref', 1, 1, 0, 1),
+                ('skip',),
+                ('skip',),
+                ('shift', 1, 'largest(A,B)'),
+                ('lift', 0, 2),
+                ('shift', 1, 'population(A,B)'),
+                ('coref', 1, 1, 0, 1),
+                ('coref', 0, 1, 0, 2),
+                ('drop', 0, 2),
+                ('drop', 0, 2),
+                ('finish',),
+                ('idle',)
+        ]
+        for action in actions:
+            successors = item.successors()
+            item = successors[action]
+        self.assertEqual(len(item.stack), 1)
+        self.assertTrue(item.queue.is_empty())
+        self.assertTrue(item.finished)
+        self.assertTrue(item.stack.head.term.equivalent(terms.from_string(
+            'answer(C, (capital(S, C), largest(P, (state(S), population(S, P)))))')))
