5dc74bee3a5738ea8b9a52c17ebedebff22b5aa6
Kilian Evang <noreply@texttheater.net>
2018-06-29 11:18:16 +0200

    Added another example to test_parseitems

--- test_parseitems.py
+++ test_parseitems.py
@@ -96,6 +96,43 @@ class ItemsTestCase(unittest.TestCase):
         target_mr = terms.from_string('answer(A,count(B,(major(B),city(B),loc(B,C),const(C,stateid(pennsylvania))),A))')
         self._test_action_sequence(words, actions, target_mr)
 
+    def test_example4(self):
+        words = ('how', 'many', 'rivers', 'do', 'not', 'traverse', 'the', 'state', 'with', 'the', 'capital', 'albany', '?')
+        target_mr = terms.from_string('answer(A,count(B,(river(B),\+ (traverse(B,C),state(C),loc(D,C),capital(D),const(D,cityid(albany,_)))),A))')
+        actions = [
+            ('skip',),
+            ('shift', 1, 'count(A,B,C)'),
+            ('coref', (1,), (3,)),
+            ('drop', (2,)),
+            ('shift', 1, 'river(A)'),
+            ('coref', (2, 1), (1,)),
+            ('drop', (2, 2)),
+            ('skip',),
+            ('shift', 1, '\+A'),
+            ('drop', (2, 2)),
+            ('shift', 1, 'traverse(A,B)'),
+            ('coref', (2, 1), (1,)),
+            ('drop', (2, 2, 1)),
+            ('skip',),
+            ('shift', 1, 'state(A)'),
+            ('coref', (2, 2, 1, 2), (1,)),
+            ('drop', (2, 2, 1)),
+            ('shift', 1, 'loc(A,B)'),
+            ('coref', (2, 2, 1, 1), (2,)),
+            ('drop', (2, 2, 1)),
+            ('skip',),
+            ('shift', 1, 'capital(A)'),
+            ('coref', (2, 2, 1, 1), (1,)),
+            ('drop', (2, 2, 1)),
+            ('shift', 1, 'const(A,cityid(albany,B))'),
+            ('coref', (2, 2, 1, 1), (1,)),
+            ('drop', (2, 2, 1)),
+            ('skip',),
+            ('finish',),
+            ('idle',),
+        ]
+        self._test_action_sequence(words, actions, target_mr)
+
     def _test_action_sequence(self, words, actions, target_mr):
         """Tests that the given action sequence is found.
 
