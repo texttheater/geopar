360f0483d71493975924c005959185abfb56df7e
Kilian Evang <noreply@texttheater.net>
2018-07-04 14:13:54 +0200

    oracles.py now using fileinput

--- oracles.py
+++ oracles.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 
+import fileinput
 import oracle
 import sys
 import terms
 
 
 if __name__ == '__main__':
-    for line in sys.stdin:
+    for line in fileinput.input():
         term = terms.from_string(line)
         words = tuple(str(w) for w in term.args[0].elements)
         mr = term.args[1]
